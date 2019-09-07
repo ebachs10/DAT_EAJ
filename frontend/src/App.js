7e6a959691e6d7c93c71931c927f0dbc5405afa7
Esben Bach Soerensen <ebachs10@gmail.com>
2019-09-07 09:08:53 +0200

    Added Journallist view and installed axios to the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import 'antd/dist/antd.css';
 
 import CustomLayout from './containers/Layout';
+import JournalList from './containers/JournalListView';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <CustomLayout>
-          
+          <JournalList/>
         </CustomLayout>
       </div>
     );
---
+++ frontend/src/containers/JournalListView.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Journal from '../components/Journal';
+
+class JournalList extends React.Component {
+    render() {
+        return(
+            <Journal />
+        );
+    }
+}
+
+export default JournalList
